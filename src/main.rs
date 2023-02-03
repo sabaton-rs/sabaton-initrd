c9bd39e300a5a962b6e6174b513383d8431b8cac
Anagha <anagha.r@acsiatech.com>
2023-02-03 15:24:59 +0530

    machine_id change

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use libc::{self, umask};
 use libcore::c_str;
+use libcore::mount::{early_mount, early_partitions};
 use std::convert::TryFrom;
 use std::ffi::CStr;
-use std::panic;
-//use std::os::unix::prelude::MetadataExt;
-use std::{alloc::System, ffi::CString, path::Path};
-use std::path::PathBuf;
-use libcore::mount::{early_partitions, early_mount};
+use std::fs::OpenOptions;
+use std::io::Write;
 use std::os::linux::fs::MetadataExt;
+//use std::os::unix::prelude::MetadataExt;
+use std::panic;
+use std::path::PathBuf;
+use std::{alloc::System, ffi::CString, path::Path};
 
 #[global_allocator]
 static GLOBAL: System = System;
@@ -21,7 +23,7 @@ fn main() {
     let path_val = CString::new("/bin").unwrap();
     let mut errors = Vec::new();
 
-    let root_dir_at_boot = unsafe{libc::opendir(c_str!("/"))};
+    let root_dir_at_boot = unsafe { libc::opendir(c_str!("/")) };
 
     let orig_root_metadata = std::fs::metadata("/").expect("Cannot get metadata of original root");
 
@@ -44,43 +46,69 @@ fn main() {
     }
 
     // Enable proper logging
-    simple_logger::SimpleLogger::new().with_utc_timestamps().with_level(log::LevelFilter::Debug).init().unwrap();
+    simple_logger::SimpleLogger::new()
+        .with_utc_timestamps()
+        .with_level(log::LevelFilter::Debug)
+        .init()
+        .unwrap();
 
     // Use the default implementation of the boothal
     let mut boot_hal = sabaton_hal::bootloader::mock::DefaultImpl;
 
     let pre_mount_time = std::time::Instant::now();
 
-    early_partitions::mount_early_partitions(&mut boot_hal).expect("Unable to mount early partitions");
+    early_partitions::mount_early_partitions(&mut boot_hal)
+        .expect("Unable to mount early partitions");
 
-      let mount_time = std::time::Instant::now();
+    let mount_time = std::time::Instant::now();
 
     let new_root_metadata = std::fs::metadata("/").expect("Cannot get metadata of new root");
 
-    if std::os::linux::fs::MetadataExt::st_dev(&new_root_metadata) != std::os::linux::fs::MetadataExt::st_dev(&orig_root_metadata) 
+    let device_unique_id = get_device_unique_id().unwrap();
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open("/tmp/device_unique_id")
+        .expect("Unable to open a writable file");
+    file.write(device_unique_id.as_bytes())
+        .expect("Unable to write device id to file");
+    println!("device_unique_id:{}", device_unique_id);
+    let res = libmount::BindMount::new("/tmp/device_unique_id", "/etc/machine-id");
+    println!("RES={}", res);
+    res.bare_mount().expect("Mount not sucessful!");
+    if std::os::linux::fs::MetadataExt::st_dev(&new_root_metadata)
+        != std::os::linux::fs::MetadataExt::st_dev(&orig_root_metadata)
     {
-        early_mount::cleanup_ramdisk(root_dir_at_boot, std::os::linux::fs::MetadataExt::st_dev(&orig_root_metadata));
+        early_mount::cleanup_ramdisk(
+            root_dir_at_boot,
+            std::os::linux::fs::MetadataExt::st_dev(&orig_root_metadata),
+        );
     }
 
     log::info!("Filesystems mounted");
-    log::info!("Pre-Mount:+{:?} Post-Mount:+{:?}",pre_mount_time - start_time, mount_time - start_time);
-
-   
-
+    log::info!(
+        "Pre-Mount:+{:?} Post-Mount:+{:?}",
+        pre_mount_time - start_time,
+        mount_time - start_time
+    );
 
     // Now we're ready to exec into the second stage init inside the root
     // partition. We expect the init to be in /sbin/init
     // initception does not attempt to mount the early devices when the -n parameter
-    // is passed.    
+    // is passed.
     let args = [SYSTEM_INIT_PATH, c_str!("-n"), std::ptr::null()];
     println!("Sabaton INIT Stage 1 complete");
     unsafe {
         libc::execv(SYSTEM_INIT_PATH, args.as_ptr());
     }
-
     log::error!("Sabaton INITRD : Failed launching system init");
     panic!("Fatal error in stage1 init");
+}
 
+// Returns the current version of the target
+pub fn get_device_unique_id() -> Result<String, std::io::Error> {
+    let device_unique_id = std::fs::read_to_string("/sys/firmware/devicetree/base/serial-number")?;
 
-    
+    Ok(device_unique_id)
 }
